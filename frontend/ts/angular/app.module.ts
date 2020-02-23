2bc5f57d78dc668c72a15184ce12378d082f6f85
Byron Farrell <byronfarrell22@gmail.com>
2020-02-23 16:36:09 +0000

    used angular cli to create login component

--- frontend/ts/angular/app.module.ts
+++ frontend/ts/angular/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { RootComponent } from './components/root/root.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    RootComponent
+    RootComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
