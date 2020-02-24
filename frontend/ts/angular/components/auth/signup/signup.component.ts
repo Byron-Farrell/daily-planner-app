fe269d96db4c5d3ba3dd3c5719c5e27140f4101d
Byron Farrell <byronfarrell22@gmail.com>
2020-02-24 13:43:07 +0000

    changed style and layout of signup component

--- frontend/ts/angular/components/auth/signup/signup.component.ts
+++ frontend/ts/angular/components/auth/signup/signup.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  public errorMessage: string;
+  public selectedQuestion: string;
+  public secretQuestions: Array<string>;
 
-  ngOnInit() {
+  constructor() {
+    this.errorMessage = '';
   }
 
+  ngOnInit() {
+    this.secretQuestions = [
+      'What\'s the name of your first pet?',
+      'What\'s the name of your best friend in high school?',
+    ]
+
+    this.selectedQuestion = this.secretQuestions[0];
+  }
+
+  public validate(): void {
+    // reset error messages
+    this.errorMessage = '';
+    this.validatePassword();
+  }
+
+  private validatePassword(): void {
+    let password1 = <HTMLInputElement> document.getElementById('password1');
+    let password2 = <HTMLInputElement> document.getElementById('password2');
+
+    if (password1.value !== password2.value) {
+      this.errorMessage = 'Passwords don\'t match';
+    }
+  }
 }
