57788980d9d62fe3315deeda5175929608228dd9
Byron Farrell <byronfarrell22@gmail.com>
2020-02-24 10:51:12 +0000

    update

--- frontend/ts/angular/app.module.ts
+++ frontend/ts/angular/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { RootComponent } from './components/root/root.component';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
 
 const routes: Routes = [
   { path: '', component: SignupComponent },
